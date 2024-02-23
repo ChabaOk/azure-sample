9cc4ce45cca7236a97ad58ca08eba34b19f53dc4
chaba_ok <chaba_ok@yahoo.com>
2024-02-23 20:41:53 +0700

    c

--- automate_cd.sh
+++ automate_cd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sudo dnf install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
-    sudo dnf install azure-cli -y
-    sudo dnf install git 
+    sudo rpm install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
+    sudo rpm install azure-cli -y
+    sudo rpm install git 
     az --version
     sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo chmod +x ./kubectl
