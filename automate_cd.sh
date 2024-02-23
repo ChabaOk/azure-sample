423911165121c6dbf198e9bf23864a18ad0c8bf9
chaba_ok <chaba_ok@yahoo.com>
2024-02-23 20:57:43 +0700

    d

--- automate_cd.sh
+++ automate_cd.sh
@@ -7,4 +7,5 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
+    sudo cp /usr/local/bin/kubectl  /sbin/
     kubectl version --client
